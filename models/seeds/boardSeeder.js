1c1fb9b0eb2799aef8cb527dca17e02a35dca9c4
Vin <purrup27@gmail.com>
2021-03-14 23:23:40 +0800

    Add boardSeeder

---
+++ models/seeds/boardSeeder.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Board = require('../board.js')
+const dbPath = 'mongodb://localhost/trello-clone'
+
+mongoose.connect(dbPath, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('db error')
+})
+
+db.once('open', () => {
+  console.log('db connected!')
+
+  for (let i = 0; i < 5; i++) {
+    Board.create({
+      title: 'Board title-' + i
+    })
+  }
+
+  console.log('Boards seeds generated!')
+})
