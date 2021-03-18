3081e5ad5725944dc252db974440e1c0fdd3bda8
Vin <purrup27@gmail.com>
2021-03-18 18:22:37 +0800

    Retrieve bpards and a single board

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const db = mongoose.connection
 db.on('error', () => console.error.bind(console, 'connection error:'))
 db.once('open', () => console.log('db successfully connected!'))
 
+app.use(express.json()) // 取代body-parser
 app.use('/', require('./routes'))
 
 app.listen(port, () => {
--- routes/boards.js
+++ routes/boards.js
@@ -1,13 +1,45 @@
 const express = require('express')
 const router = express.Router()
+const Boards = require('../models/board.js')
+const mongoose = require('mongoose')
 
 // 列出全部boards
-router.get('/', (req, res) => {
-  res.send('send every boards')
+router.get('/', async (req, res) => {
+  // Board.find({members: user_id}) // where user_id is the ID of the user
+  const boards = await Boards.find({})
+  res.send(boards)
 })
-// 取得單一board的頁面
-router.get('/:id', (req, res) => {
-  res.send('send a certain board')
+// 取得單一board
+router.get('/:id', async (req, res) => {
+  // 取得board之下的lists and cards
+  const board = await Boards.aggregate([
+    { $match: { _id: mongoose.Types.ObjectId(req.params.id) } },
+    {
+      $lookup: {
+        from: 'lists',
+        let: { boardId: '$_id' }, // 定義boardId為Boards model中的objectId
+        pipeline: [
+          {
+            $match: { $expr: { $eq: ['$boardId', '$$boardId'] } }
+          },
+          {
+            $lookup: {
+              from: 'cards',
+              let: { listId: '$_id' }, // 定義變數：listId 為Lists model中的objectId
+              pipeline: [
+                {
+                  $match: { $expr: { $eq: ['$listId', '$$listId'] } }
+                }
+              ],
+              as: 'cards'
+            }
+          }
+        ],
+        as: 'lists'
+      }
+    }
+  ])
+  res.send(board)
 })
 // 修改單一board
 router.post('/:id', (req, res) => {
