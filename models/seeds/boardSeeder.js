a012110060e102efaea9fa9b2325cad08f11940e
Vin <purrup27@gmail.com>
2021-03-19 18:34:58 +0800

    Edit board model and boardSeeder

--- models/seeds/boardSeeder.js
+++ models/seeds/boardSeeder.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Board = require('../board.js')
+const User = require('../user.js')
 const dbPath = 'mongodb://localhost/trello-clone'
 
 mongoose.connect(dbPath, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -9,14 +10,21 @@ db.on('error', () => {
   console.log('db error')
 })
 
-db.once('open', () => {
+db.once('open', async () => {
   console.log('db connected!')
+  const users = await User.find({})
 
   for (let i = 0; i < 5; i++) {
     Board.create({
-      title: 'Board title-' + i
+      title: 'Board title-' + i,
+      userId: getRandomUserId(users)
     })
   }
 
   console.log('Boards seeds generated!')
 })
+
+function getRandomUserId(users) {
+  const randomUserIndex = Math.floor(Math.random() * Math.floor(users.length))
+  return users[randomUserIndex]._id
+}
