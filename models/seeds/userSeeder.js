196bff29677598c59bd4dd132bd178113c3eeb03
Vin <purrup27@gmail.com>
2021-04-06 23:52:47 +0800

    Change way of creating user seeds

--- models/seeds/userSeeder.js
+++ models/seeds/userSeeder.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const User = require('../user.js')
 const dbPath = 'mongodb://localhost/trello-clone'
+const bcrypt = require('bcrypt')
 
 mongoose.connect(dbPath, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -9,14 +10,14 @@ db.on('error', () => {
   console.log('db error')
 })
 
-db.once('open', () => {
+db.once('open', async () => {
   console.log('db connected!')
-
+  await User.collection.drop()
   for (let i = 0; i < 5; i++) {
     User.create({
-      name: 'User No.' + i,
-      email: 'User email-' + i,
-      password: 'User password-' + i
+      name: 'user' + i,
+      email: 'email' + i + '@test.com',
+      password: bcrypt.hashSync(`password${i}`, bcrypt.genSaltSync(10))
     })
   }
 
