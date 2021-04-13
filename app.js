3de298008c38c4ad02991335db135d4b5a049019
Vin <purrup27@gmail.com>
2021-04-14 00:41:53 +0800

    Modify origin of cors options

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== 'production') {
 const corsOptions = {
   origin: [
     'https://trello-clone-frontend-2021.herokuapp.com/',
-    'https://trello-clone-backend-2021.herokuapp.com/'
+    'http://localhost:8080'
   ],
   methods: ['GET', 'PUT', 'POST', 'OPTIONS', 'PATCH', 'DELETE', 'HEAD'],
   credentials: true
